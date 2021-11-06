7359500b1acd1b95bb404cd1d00c14c235b27989
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-11-06 17:07:19 -0400

    completed lab

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,6 +1,9 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
 
 import java.util.Date;
@@ -16,10 +19,23 @@ public class AnimalFactoryTest {
     public void testCreateDog() {
         //given
         String expectedName = "Rocco";
-
+        Date expectedDate = new Date();
         //when
-        AnimalFactory.createDog("Rocco", new Date());
+        Dog actualDog = AnimalFactory.createDog(expectedName, expectedDate);
         //then
+        Assert.assertEquals(expectedName, actualDog.getName());
+        Assert.assertEquals(expectedDate, actualDog.getBirthDate());
+    }
 
+    @Test
+    public void testCreateCog() {
+        //given
+        String expectedName = "Luna";
+        Date expectedDate = new Date();
+        //when
+        Cat actualCat = AnimalFactory.createCat(expectedName, expectedDate);
+        //then
+        Assert.assertEquals(expectedName, actualCat.getName());
+        Assert.assertEquals(expectedDate, actualCat.getBirthDate());
     }
 }
