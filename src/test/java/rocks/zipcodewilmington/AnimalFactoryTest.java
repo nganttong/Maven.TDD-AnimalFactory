bbd118cffd0462b3117da3298a45d770f1ad4ff2
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-11-06 16:58:57 -0400

    work in progress

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,25 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+
+    @Test
+    public void testCreateDog() {
+        //given
+        String expectedName = "Rocco";
+
+        //when
+        AnimalFactory.createDog("Rocco", new Date());
+        //then
+
+    }
 }
